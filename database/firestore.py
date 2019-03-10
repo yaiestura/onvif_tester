3c38010e4c895c8f1796664e95a8e05864228815
yaiestura <patsvetov@edu.hse.ru>
2019-03-10 17:37:12 +0300

    try add user to db

--- database/firestore.py
+++ database/firestore.py
@@ -7,3 +7,10 @@ cred = credentials.Certificate('onviftester.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+
+document = db.collection(u'users').document(u'ArtemyMagarin')
+doc_ref.set({
+    u'first': u'Artemy',
+    u'last': u'Magarin',
+    u'born': 1998
+})
