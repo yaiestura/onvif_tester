6be7e67927aab75d039ba87f5c2b01025c5ae637
Artemy Magarin <magar.art@yandex.ru>
2019-03-12 01:41:47 +0300

    added spa build

---
+++ templates/build/precache-manifest.0f53b823f14f00be32aae9285c97b0e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "0defb8cf05c380f25ecb",
+    "url": "/static/js/main.b4834286.chunk.js"
+  },
+  {
+    "revision": "37cdf8112cce862c95cd",
+    "url": "/static/js/2.cfa4f54e.chunk.js"
+  },
+  {
+    "revision": "0defb8cf05c380f25ecb",
+    "url": "/static/css/main.85a57b36.chunk.css"
+  },
+  {
+    "revision": "37cdf8112cce862c95cd",
+    "url": "/static/css/2.bb2e39e2.chunk.css"
+  },
+  {
+    "revision": "6b8106cf9c0a3a71871d76a9a23b1cdd",
+    "url": "/index.html"
+  }
+];
